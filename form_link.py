315c1825780051813f6f1682a764aa21c7309084
Nathan Decaux <nathan.decaux@imt-atlantique.net>
2019-05-30 17:36:26 +0300

    Pre working form

--- form_link.py
+++ form_link.py
@@ -23,6 +23,11 @@ STATICFILES_DIRS = (
     os.path.join(os.path.dirname(__file__),'static'),
 )
 
+choices_po = dict()
+
+class SelectFieldNoValidation(SelectField):
+    def pre_validate(self, form):
+        pass
 
 class LinkForm(Form):
     txee = DecimalField('Tx Level (dBm)')
@@ -30,32 +35,35 @@ class LinkForm(Form):
     gbe = DecimalField('Antenna Gain B (dBi)')
     xe = DecimalField('Rx Antenna Coordinates ({x,y})')
     ye = DecimalField()
-    fe = DecimalField('Frequency (GHz)')
     ele = DecimalField('Rx Antenna Elevation (degrees)')
     polar = SelectField('Polar',choices=[('h', 'Horizontal'), ('v', 'Vertical')])
     rre = DecimalField('Rainrate (mm/h)')
     p_entry = DecimalField('Antenna Gain B (dBi)')
+    submit_button = SubmitField('Submit Form')
+
 
 class EquipForm(Form):
     cb0 = SelectField('XPIC', choices=[('3','----'),('1', 'Yes'), ('0', 'No')],)
-    cb1 = SelectField('Equipment',choices=[('0', '----')],validators=())
+    cb1 = SelectField('Equipment',choices=[('0', '----')])#[('0', '----')],validators=())
     fe = SelectField('Frequency (GHz)',choices=[('0', '----')])
     carde = SelectField('Modem Card + ODU',choices=[('0', '----')])
     cpe = SelectField('Bandwidth (MHz)',choices=[('0', '----')])
     ref_mod = SelectField('Reference Modulation', choices=[('0', '----')])
     am = SelectField('Adaptative Modulation', choices=[('3','----'),('1', 'Yes'), ('0', 'No')])
+    def validate(self):
+        return True
 
 class MetricForm(Form):
     rainp = BooleanField('Rain attenuation')
     modp = BooleanField('Modulation / Distance')
     availp = BooleanField('Availability / Distance')
-
+    def validate(self):
+        return True
 class GlobalForm(Form):
     lp = FormField(LinkForm,label="Link Profil")
     ep = FormField(EquipForm, label="Equipment Profil")
     mf = FormField(MetricForm, label="Graphs")
-    fe = SelectField('Frequency (GHz)', choices=[('0', '----')])
-    submit_button = SubmitField('Submit Form')
+
 
 if __name__ == '__main__':
     app = Flask(__name__)
@@ -70,7 +78,7 @@ if __name__ == '__main__':
 
     @flask_sijax.route(app,'/')
     def index():
-        choices = dict()
+
         #form = GlobalForm()
         form = GlobalForm()
         def cb0Ch(obj_response,xpic):
@@ -84,10 +92,11 @@ if __name__ == '__main__':
             i=0
             choix = list()
             for val in db.tables():
-                obj_response.html_append('#ep-cb1', '<option id='+str(i)+' value='+val+' >'+val+'</option>')
+                obj_response.html_append('#ep-cb1', '<option id='+str(i)+' value='+str(val)+' >'+val+'</option>')
                 choix.append((str(val),str(val)))
                 i=i+1
-            choices['cb1']=choix
+            form.ep.cb1.choices = choix
+            choices_po['cb1']=choix
 
 
 
@@ -211,15 +220,27 @@ if __name__ == '__main__':
                     i) + ' value=' + modulation + ' >' + modulation + '</option>')
 
             form.ep.ref_mod.choices = choix
+        # def submittion(obj_response,xpic,equi,freq,carde,bandw,mod):
+        #     form.ep.cb1.choices = [(equi,equi)]
+        #     print(form.ep.cb1.choices)
 
-        if form.validate_on_submit():
+        if g.sijax.is_sijax_request:
+            g.sijax.register_callback('cb0', cb0Ch)
+            g.sijax.register_callback('cb1', equiCh)
+            g.sijax.register_callback('fe', freqCh)
+            g.sijax.register_callback('carde', cardCh)
+            g.sijax.register_callback('cpe', bandwCh)
+            # g.sijax.register_callback('click',submittion)
+            return g.sijax.process_request()
+
+
+        if form.is_submitted():
+            print(form.lp.validate(form))
             link = form.lp
-            ep = form.lp
+            ep = form.ep
             tx1 = float(form.lp.txee.data)
             g1a = float(link.gae.data)
             g1b = float(link.gbe.data)
-            d = np.arange(0.001, 20, 0.001)
-            f = float(link.fe.data)
             el = float(link.ele.data)
             geoloc = (0, 0)
             tau = 45  # float(form.polar.data)
@@ -227,35 +248,30 @@ if __name__ == '__main__':
             p0 = float(link.p_entry.data)
             xpic = ep.cb0.data
             equip = ep.cb1.data
-            freq = ep.fe.data
+            freq = float(ep.fe.data)
             card = ep.carde.data
             bw = ep.cpe.data
-            mod = ep.ref_mode.data
+            mod = ep.ref_mod.data
             am = ep.am.data
-            print(form.mf.modp.data)
+            html = ''
+            if(form.mf.rainp.data):
+                d = np.arange(0.001, 20, 0.001)
+                p = plt.figure(title='Rain-caused exceeded attenuation according to the distance',x_axis_label = 'Distance (km)',y_axis_label = 'Attenuation (dB)')
+                p.line(d, itur.models.itu530.rain_attenuation(0, 0, d, freq, el, 0.01, tau, rr), line_width=2)
+                p.add_tools(HoverTool())
+                html = file_html(p, CDN, "my plot")
+            if(form.mf.modp.data):
+                d = np.arange(0.001, 20, 0.001)
+                p = plt.figure(title='Rain-caused exceeded attenuation according to the distance',
+                               x_axis_label='Distance (km)', y_axis_label='Attenuation (dB)')
+                p.line(d, itur.models.itu530.rain_attenuation(0, 0, d, freq, el, 0.01, tau, rr), line_width=2)
+                p.add_tools(HoverTool())
+                html = html+file_html(p, CDN, "my plot")
 
-        if g.sijax.is_sijax_request:
-            g.sijax.register_callback('cb0',cb0Ch)
-            g.sijax.register_callback('cb1',equiCh)
-            g.sijax.register_callback('fe', freqCh)
-            g.sijax.register_callback('carde', cardCh)
-            g.sijax.register_callback('cpe', bandwCh)
-            #g.sijax.register_callback('click',submitted)
-            return g.sijax.process_request()
+            print('Ca va ?')
+            return html
 
 
-
-        #     p = plt.figure(title='Rain-caused exceeded attenuation according to the distance',x_axis_label = 'Distance (km)',y_axis_label = 'Attenuation (dB)')
-        #     p.line(d, itur.models.itu530.rain_attenuation(0, 0, d, f, el, 0.01, tau, rr), line_width=2)
-        #     p.add_tools(HoverTool())
-        #     html = file_html(p, CDN, "my plot")
-        #     # plt.plot(itur.models.itu530.rain_attenuation(geoloc[0],geoloc[1], d, f2, el, 0.01,tau,rr),label='66GHz')
-        #     # plt.xlabel('Distance (km)')
-        #     # plt.ylabel('Attenuation (dB)')
-        #     # plt.legend()
-        #     #plt.title()
-
-            #return html
 
         return render_template('index.html', form=form)
 
